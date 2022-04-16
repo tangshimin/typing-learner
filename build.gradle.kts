8e8b34e53498101ba3e0e19f5c92e01a09e3e77d
shimin tang <tang_shimin@qq.com>
2022-04-16 20:28:27 +0800

    修复 (issue #4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.apache.opennlp:opennlp-tools:1.9.4")
     implementation("org.apache.pdfbox:pdfbox:2.0.24")
     implementation("com.h2database:h2:2.1.210")
+    implementation("org.slf4j:slf4j-parent:2.0.0-alpha7")
     implementation(files("lib/ebml-reader-0.1.0.jar"))
     implementation(files("lib/subtitleConvert-1.0.2.jar"))
 }
