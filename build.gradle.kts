5e617e3ce713840056f049bf6d1c983aacca4c4e
shimin tang <tang_shimin@qq.com>
2022-12-08 17:00:32 +0800

    升级 Compose 到 1.3.0-beta04-dev885， 修复了和输入法相关的 bug,中文输入法的提示在光标附近了。

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.20"
-    id("org.jetbrains.compose") version "1.3.0-beta04-dev871"
+    id("org.jetbrains.compose") version "1.3.0-beta04-dev885"
     kotlin("plugin.serialization") version "1.7.20"
 }
 
